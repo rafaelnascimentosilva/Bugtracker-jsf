62a3ae1e6d77061be5a9918ba5cb24f766e73780
rafaelnascimentosilva <rafaelnasc.silv@gmail.com>
2017-04-25 06:30:23 -0300

    Implementando paginação sob demanda com LazyDataModel

---
+++ src/br/com/triadworks/bugtracker/datamodel/BugDataModel.java
@@ -0,0 +1,44 @@
+package br.com.triadworks.bugtracker.datamodel;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
+
+import br.com.triadworks.bugtracker.dao.BugDao;
+import br.com.triadworks.bugtracker.modelo.Bug;
+
+@ViewScoped
+@ManagedBean
+public class BugDataModel extends LazyDataModel<Bug> {
+
+	
+
+	@ManagedProperty("#{bugDao}")
+	private BugDao dao;
+
+	@PostConstruct
+	public void init() {
+		this.setRowCount(dao.contaTodos());
+	}
+
+	@Override
+	public List<Bug> load(int inicio, int quantidade, String campoDeOrdenacao, SortOrder sentidoDeOrdenacao,
+			Map<String, Object> filtros) {
+
+		return dao.listaPaginada(inicio, quantidade);
+	}
+	public BugDao getDao() {
+		return dao;
+	}
+
+	public void setDao(BugDao dao) {
+		this.dao = dao;
+	}
+}
